b4e6f15fde2838baad3ec6d492626dd71c80079b
joy <blackbearmode1@gmail.com>
2024-04-01 12:11:58 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(superbowlResults) {
+    const winningYear = superbowlResults.find(result => result.result === "W");
+    return winningYear ? winningYear.year : undefined;
+}
+
